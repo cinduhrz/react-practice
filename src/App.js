ac5182adb54c3fa146f5795a3258397018321da5
Cindy Wong <cindywongdev@gmail.com>
2022-12-14 20:55:44 -0500

    created board, header, and player components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Heading from './components/Heading'
+import Header from './components/Header';
+import Player from './components/Player';
+import Board from './components/Board';
 
 function App(){
   return (<h1>React Tac Toe</h1>)
